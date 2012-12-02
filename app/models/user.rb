9b09913a28ff0511bf0118902844911042afb106
Ajmal Afif <ajmalafif@gmail.com>
2012-12-03 06:26:55 +0800

    Stuck with views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
 
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :avatar, :name, :email, :password, :password_confirmation, :remember_me
