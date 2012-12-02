10e499c24f9e261736547d7cd42fd02d21bf3ec0
Ajmal Afif <ajmalafif@gmail.com>
2012-12-03 03:55:16 +0800

    added something in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+
+  before_filter :authenticate_user!, :except => [:some_action_without_auth]
+
   def index
+    @user = User.all
   end
 
   def show
+     @user = User.find(params[:id])
   end
 
   def new
