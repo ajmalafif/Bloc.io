32797e73f74cbdc214297bec849cd748f85b9984
Ajmal Afif <ajmalafif@gmail.com>
2012-12-03 04:58:03 +0800

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121202203530) do
+ActiveRecord::Schema.define(:version => 20121202205342) do
 
   create_table "markets", :force => true do |t|
     t.string   "name"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(:version => 20121202203530) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
   end
 
   create_table "stores", :force => true do |t|
