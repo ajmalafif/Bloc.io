14414fc31b437fc354e99ca2a4260bd9a3dc9823
Ajmal Afif <ajmalafif@gmail.com>
2012-12-02 23:50:06 +0800

    initial first commit bare app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazaar::Application.config.secret_token = '548b7ffd53def0a71912d142e1245fd4be6f117f3d85b0280adcf8650cf63340735873b5af7786b2957aac7c95b86303a3e35f901047204eba380e2b383ec1e2'
