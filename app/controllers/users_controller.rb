9bc4130bb758a4e2a84e6d5dc5e1b41fe88e6be3
Ajmal Afif <ajmalafif@gmail.com>
2012-12-03 19:22:59 +0800

    trying @users.error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-  end
+  # def create
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+ def show_errors(obj)
+    return_string = "<div class='errors'><p>There were some errors:</p><ul>"
+    obj.errors.each do |key, value|
+     return_string += "<li>#{key.capitalize} #{value}</li>"
+    end
+    return_string += "</ul></div>"
+    return_string.html_safe
+  end
+
 end
